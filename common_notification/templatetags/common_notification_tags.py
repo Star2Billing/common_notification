297b1d28e74dd0ec973e8fba89f2972e234323f2
Shrenik Patel <shrenik181986@gmail.com>
2012-11-26 15:24:57 +0530

    added get_notice_count tag

--- common_notification/templatetags/common_notification_tags.py
+++ common_notification/templatetags/common_notification_tags.py
@@ -15,6 +15,7 @@
 from django.template.defaultfilters import register
 from django.utils.translation import ugettext_lazy as _
 from django.conf import settings
+from common_notification.views import notice_count
 
 
 @register.filter(name='notification_style')
@@ -38,3 +39,9 @@ def notification_status(val):
         return _('New')
     else:
         return _('Read')
+
+
+@register.simple_tag(name='get_notice_count')
+def get_notice_count(request):
+    """tag to display notice count"""
+    return notice_count(request)
